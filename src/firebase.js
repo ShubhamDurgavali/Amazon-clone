ee7c1b90c77230a2dce9394600ad6e456be6506d
ShubhamDurgavali <shubham.durgavali99@gmail.com>
2021-07-26 22:25:19 +0530

    Added all the files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCcSWqVz8lFZK8DHNmrhawT_8yyzcxdMz8",
+    authDomain: "challenge-bae99.firebaseapp.com",
+    projectId: "challenge-bae99",
+    storageBucket: "challenge-bae99.appspot.com",
+    messagingSenderId: "1032091658746",
+    appId: "1:1032091658746:web:25211034ebeabf79721c43",
+    measurementId: "G-9NL6L9VJQN"
+  };
